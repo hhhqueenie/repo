d697eea24f249ece386b4017e069308a9b5bd826
Ankie <1760986462@qq.com>
2024-05-02 04:41:32 +0200

    Explanation

---
+++ model/explaination.py
@@ -0,0 +1,30 @@
+from lime.lime_text import LimeTextExplainer
+import numpy as np
+import re
+import json
+
+class Explainer():
+
+    def __init__(self, classifier):
+        self.classifier = classifier
+        label2ind = json.load(open('data/files/label2ind.json', 'r', encoding='utf-8'))
+        self.ind2label = {value: key for key, value in label2ind.items()}
+
+    def predict_proba(self, texts):
+        labels, probabilities = self.classifier.predict(texts, k=len(self.classifier.get_labels()))
+        mapped_labels = [[self.ind2label[label] for label in label_list] for label_list in labels]
+        mapped_probabilities = [dict(zip(mapped_label_list, prob_list)) for mapped_label_list, prob_list in zip(mapped_labels, probabilities)]
+        return np.array([[prob_dict.get(class_name, 0) for class_name in self.ind2label.values()] for prob_dict in mapped_probabilities])
+
+    def split_rule(self, text):
+        sentences = re.split('(。|！|\!|\.|？|\?)',text)
+        new_sents = []
+        for i in range(int(len(sentences)/2)):
+            sent = sentences[2*i] + sentences[2*i+1]
+            new_sents.append(sent)
+        return new_sents
+
+    def get_explaination(self, text): # text: need to be preprocessed and cut before passing in
+        model = LimeTextExplainer(split_expression=self.split_rule, class_names=list(self.ind2label.values()))
+        exp = model.explain_instance(text, self.predict_proba, num_features=1000, top_labels=3)
+        return exp
